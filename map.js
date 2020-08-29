56f2677691c46d7ae6daa6527b9455a6198129b3
lmateus <lsmateusb@gmail.com>
2020-08-29 11:14:51 -0500

    Actualizacion_Json

--- map.js
+++ map.js
@@ -1,14 +1,14 @@
 const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
-let mymap = L.map('mapid').setView([4.5818, -74.14], 10);
+let mymap = L.map('mapid').setView([4.644692, -74.079616], 12);
 
 
 L.tileLayer(tilesProvider, {maxZoom: 18,}).addTo(mymap);
 
 function Popup (feature,layer){
-	layer.bindPopup("<h2>Info del evento</h2><h3>Descripción</h3><p>" + feature.properties.Descripcio +"</p><h3>Latitud</h3><p>" + feature.properties.Lat_desliz +"</p><h3>Longitud</h3><p>" + feature.properties.Long_desli +"</p>")
+	layer.bindPopup(`<h4>Profundidad (m)</h4>${feature.properties.PROFUNDIDA}<h4>Fuente</h4>${feature.properties.ID__FUENTE}`)
 };
-var marker = L.marker([4.581855, -74.141236]).addTo(mymap);
-L.geoJson(DeslizamientoSismo_Col,{
+
+L.geoJson(BDG,{
 	onEachFeature: Popup
 }).addTo(mymap);
